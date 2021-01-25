8f034d1e62104e00003966af6906fe854ba2e05a
lesismal <40462947+lesismal@users.noreply.github.com>
2021-01-25 17:02:03 +0800

    Update tool.go

--- tool.go
+++ tool.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// TravelDir .
 func TravelDir(pathname string, dst string) error {
 	rd, err := ioutil.ReadDir(pathname)
 	for _, fi := range rd {
@@ -29,6 +30,44 @@ func TravelDir(pathname string, dst string) error {
 	return err
 }
 
+func genPages(rootPath string) (string, string) {
+	retStr := ""
+	divRetStr := ""
+	var traval func(string)
+	traval = func(pathname string) {
+		retStr += "\n"
+		rd, _ := ioutil.ReadDir(pathname)
+		for _, fi := range rd {
+			if fi.IsDir() {
+				fmt.Printf("[%s]\n", pathname+fi.Name())
+				traval(pathname + fi.Name() + "/")
+			} else {
+				// fmt.Println(fi.Name(), "->", fi.Name())
+
+				retStr += fmt.Sprintf(`
+				{
+					dst: "%v",
+					url: "%v",
+					lazy: true,
+				},`, fi.Name(), (pathname + fi.Name())[len(rootPath):])
+				divRetStr += fmt.Sprintf(`<div id="%v"></div>%v`, (pathname + fi.Name())[len(rootPath):], "\n")
+			}
+		}
+		retStr += "\n"
+	}
+	traval(rootPath)
+	retStr = "[\n" + retStr + "\n]"
+	divRetStr = "<div>\n" + divRetStr + "\n</div>"
+	return retStr, divRetStr
+}
+
 func main() {
-	TravelDir("./static/oldpage/", "./static/page/")
+	// TravelDir("./src/page/", "./static/page/")
+
+	ret, divRet := genPages("./src/page/")
+	fmt.Println("--------------------------------")
+	fmt.Println(ret)
+	fmt.Println("--------------------------------")
+	fmt.Println(divRet)
+	fmt.Println("--------------------------------")
 }
